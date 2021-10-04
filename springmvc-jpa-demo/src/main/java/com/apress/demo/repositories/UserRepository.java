97465c383b82cf6737dd6a059f08ca53b0fcadca
Cristhian Camilo Gómez Narváez <Cristhian.C.Gomez.-ND@disney.com>
2021-10-04 01:45:53 -0500

    initial commit

---
+++ springmvc-jpa-demo/src/main/java/com/apress/demo/repositories/UserRepository.java
@@ -0,0 +1,6 @@
+package com.apress.demo.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.apress.demo.domain.User;
+public interface UserRepository extends JpaRepository<User, Integer>
+{
+}
