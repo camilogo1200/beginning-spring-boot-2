78372ca92bb9564e68f2de42a0d54f847d9ded12
Cristhian Camilo Gómez Narváez <Cristhian.C.Gomez.-ND@disney.com>
2021-10-08 01:49:16 -0500

    first demo SpringBoot2,  MySQL, JPA, ThymeLeaf

--- springmvc-jpa-demo/src/main/java/com/apress/demo/repositories/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.apress.demo.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.apress.demo.domain.User;
-public interface UserRepository extends JpaRepository<User, Integer>
-{
-}
